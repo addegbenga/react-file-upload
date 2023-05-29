1d862e108cd6e9c3bf4ae9fc3d4e577d4a8fec0f
Adeyemi <addegbenga@gmail.com>
2023-05-29 13:33:52 +0100

    patch

--- versioning.js
+++ versioning.js
@@ -1,6 +1,5 @@
 const fs = require("fs");
 const semver = require("semver");
-
 // Read the current version from package.json
 const packageJson = fs.readFileSync("./package.json");
 const { version } = JSON.parse(packageJson);
@@ -8,18 +7,10 @@ const { version } = JSON.parse(packageJson);
 // Get the commit message from the command line argument
 const commitMessage = process.argv[2];
 
-// Extract the release type and commit message from the commit message
+// Extract the release type from the commit message
 const releaseTypeMatch = commitMessage.match(/^(patch|minor|major):\s*(.*)/);
-const releaseType = releaseTypeMatch ? releaseTypeMatch[1] : null;
-const commitMessageText = releaseTypeMatch ? releaseTypeMatch[2] : null;
 
-// Check if a valid release type and commit message are provided
-if (!releaseType || !commitMessageText) {
-  console.error(
-    'Invalid commit message format. Please use the format "<release-type>: <commit-message>"'
-  );
-  process.exit(1);
-}
+const releaseType = releaseTypeMatch ? releaseTypeMatch[1] : null;
 
 // Increment the version based on the release type
 const nextVersion = semver.inc(version, releaseType);
@@ -36,4 +27,3 @@ const updatedPackageJson = { ...JSON.parse(packageJson), version: nextVersion };
 fs.writeFileSync("./package.json", JSON.stringify(updatedPackageJson, null, 2));
 
 console.log(`Version updated: ${version} → ${nextVersion}`);
-console.log(`Commit message: ${commitMessageText}`);
